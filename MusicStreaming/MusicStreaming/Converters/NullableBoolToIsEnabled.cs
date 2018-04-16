d158e5078904453c0f6b6e4dbe4e97fd878097f9
Wajdi Gharsalli <wajdi.gharsalli1@gmail.com>
2018-04-15 22:21:42 -0400

    ajout du packages

---
+++ MusicStreaming/MusicStreaming/Converters/NullableBoolToIsEnabled.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MusicStreaming.Converters
+{
+    class NullableBoolToIsEnabled : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? boolValue = (bool?)value;
+            if ( boolValue == null)
+                return false;
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true)
+                return true;
+            return null;
+        }
+    }
+}
---
+++ MusicStreaming/MusicStreaming/Converters/NullableBoolToPlayPauseText.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MusicStreaming.Converters
+{
+    class NullableBoolToPlayPauseText : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = "Play";
+            if ((bool?)value == true)
+                text = "Pause";
+            return text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "Pause")
+                return true;
+            return false;
+        }
+    }
+}
